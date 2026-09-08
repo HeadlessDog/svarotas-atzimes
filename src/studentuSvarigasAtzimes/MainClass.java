178f36d56cf974599271195be1e3e6cbc1232cc1
HeadlessDog <headlessdog1@gmail.com>
2026-09-08 09:58:44 +0300

    Pievienota isInteger funkcija, kurā var ievadīt String un radix. Funkcija atgriež true vai false ja String atblist radix.
    Pievienoti visi plānotie globālie mainīgie.
    
    Izveidots skolēnu skaita datu ievades cikls, kas atkārtoti jautā ievadīt
    skolēnu skaitu līdz skaits ir integer vērtība no 1 līdz 100000.
    Kad mainīgais ir pareizs, skaits tiek saglabāts sklSk mainīgā.
    
    Signed-off-by: HeadlessDog <headlessdog1@gmail.com>

--- src/studentuSvarigasAtzimes/MainClass.java
+++ src/studentuSvarigasAtzimes/MainClass.java
@@ -1,10 +1,46 @@
 package studentuSvarigasAtzimes;
 
+import java.util.Scanner;
+
 public class MainClass {
+	
+	public static boolean isInteger(String s, int radix) {
+	    if(s.isEmpty()) return false;
+	    for(int i = 0; i < s.length(); i++) {
+	        if(i == 0 && s.charAt(i) == '-') {
+	            if(s.length() == 1) return false;
+	            else continue;
+	        }
+	        if(Character.digit(s.charAt(i),radix) < 0) return false;
+	    }
+	    return true;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int pdSk;
+		int sklSk = 0;
+		int[] pdWg;
+		int[] pdRez;
+		String[] sklName;
+		
+		System.out.println("Jūs esat pieslēdiez gala atzīmju kalkulatoram.\n");
+		boolean validInt;
+		do {
+			System.out.println("Ievadiet skolēnu skaitu: ");
+			String tempInput = sc.next();
+			
+			if(isInteger(tempInput, 10))
+				sklSk = Integer.parseInt(tempInput);
+			
+			validInt = (sklSk < 1 || sklSk > 100000) ? false : true;
+				
+			if(!validInt)
+				System.out.println("Nedarīga vērtība!");
+		}while(!validInt);
+		
+		sc.close();
 	}
 
 }
