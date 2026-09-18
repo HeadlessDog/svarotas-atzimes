c110f3b4e00afe12f594d171ac3ec7a297e4e475
HeadlessDog <headlessdog1@gmail.com>
2026-09-18 08:43:21 +0300

    Pārvietoju skolēnu skaita izvēles ciklu uz Method_class un pārveidoju to uz metodu. Programma pašlaik nefunkcionāla

--- src/studentuSvarigasAtzimes/MainClass.java
+++ src/studentuSvarigasAtzimes/MainClass.java
@@ -1,22 +1,10 @@
 package studentuSvarigasAtzimes;
 
 import java.text.DecimalFormat;
+import studentuSvarigasAtzimes.Method_class;
 import java.util.Scanner;
 
 public class MainClass {
-	
-	//Funkcija kas pārbauda vai ievadītais ir integer un atgriež true or false
-	public static boolean isInteger(String s, int radix) {
-	    if(s.isEmpty()) return false;
-	    for(int i = 0; i < s.length(); i++) {
-	        if(i == 0 && s.charAt(i) == '-') {
-	            if(s.length() == 1) return false;
-	            else continue;
-	        }
-	        if(Character.digit(s.charAt(i),radix) < 0) return false;
-	    }
-	    return true;
-	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -31,20 +19,8 @@ public class MainClass {
 		String[] sklName;
 		
 		System.out.println("Jūs esat pieslēdiez gala atzīmju kalkulatoram.\n");
-		boolean validInt;
-		//Skolēnu skaita ievade ar validāciju
-		do {
-			System.out.println("Ievadiet skolēnu skaitu: ");
-			String tempInput = sc.next();
-			
-			if(isInteger(tempInput, 10))
-				sklSk = Integer.parseInt(tempInput);
-			
-			validInt = (sklSk < 1 || sklSk > 100000) ? false : true;
-				
-			if(!validInt)
-				System.out.println("Nedarīga vērtība!");
-		}while(!validInt);
+		
+		sklSk = Method_class.skolSk();
 		sklName = new String[sklSk];
 		finalRez = new double[sklSk];
 		
--- src/studentuSvarigasAtzimes/Method_class.java
+++ src/studentuSvarigasAtzimes/Method_class.java
@@ -1,7 +1,40 @@
 package studentuSvarigasAtzimes;
 
-public class Method_class {
+import java.util.Scanner;
 
+public class Method_class {
+	static Scanner sc = new Scanner(System.in);
+	static boolean validInt;
+	//Funkcija kas pārbauda vai ievadītais ir integer un atgriež true or false
+		public static boolean isInteger(String s, int radix) {
+		    if(s.isEmpty()) return false;
+		    for(int i = 0; i < s.length(); i++) {
+		        if(i == 0 && s.charAt(i) == '-') {
+		            if(s.length() == 1) return false;
+		            else continue;
+		        }
+		        if(Character.digit(s.charAt(i),radix) < 0) return false;
+		    }
+		    return true;
+		}
+		
+		//Skolēnu skaita ievade ar validāciju
+		public static int skolSk() {
+			int sklSk =0;
+				do {
+					System.out.println("Ievadiet skolēnu skaitu: ");
+					String tempInput = sc.next();
+					
+					if(isInteger(tempInput, 10))
+						sklSk = Integer.parseInt(tempInput);
+					
+					validInt = (sklSk < 1 || sklSk > 100000) ? false : true;
+						
+					if(!validInt)
+						System.out.println("Nedarīga vērtība!");
+				}while(!validInt);
+				return sklSk;
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
