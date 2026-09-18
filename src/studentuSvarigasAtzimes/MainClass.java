bea753d22d3c0b16d56ca5e18eb311e6872814c1
HeadlessDog <headlessdog1@gmail.com>
2026-09-18 09:12:14 +0300

    Pārveidoju visus atlikušos ciklus par metodēm iekš Method_class

--- src/studentuSvarigasAtzimes/MainClass.java
+++ src/studentuSvarigasAtzimes/MainClass.java
@@ -29,75 +29,14 @@ public class MainClass {
 		pdRez = new int[sklSk][pdSk];
 		
 		Method_class.smagumuIevade(pdSk, pdWg);
-		//Skolēnu vārdu ievade ar blank validāciju
-		int maxNameLength =0;
-		sc.nextLine();
-		boolean validString;
-		for(int j=0;j<sklSk;j++) {
-			do {
-				System.out.println("Ievadiet "+(j+1)+". skolēna vārdu");
-				String tempInput = sc.nextLine();	
-				
-				validString = !tempInput.isBlank();
-				if(!validString)
-					System.out.println("Nedarīga vērtība!");
-				else {
-					sklName[j] = tempInput; 
-					maxNameLength = Math.max(maxNameLength, tempInput.length());
-				}
-			}while(!validString);
-		}
 		
-		//Rezultātu ievade
-		for(int i=0;i<sklSk;i++)
-		{
-			for(int j=0;j<pdSk;j++)
-			{
-				System.out.println("Ievadi skolēna: "+sklName[i]+" atzīmi par "+(j+1)+". testu:\n"
-						+ "(svars "+pdWg[j]+")");
-				
-				pdRez[i][j] = 0;
-				do {
-					String tempInput = sc.next();
-					
-					if(isInteger(tempInput, 10))
-						pdRez[i][j] = Integer.parseInt(tempInput);
-					
-					validInt = (pdRez[i][j] < 1 || pdRez[i][j] > 10) ? false : true;
-						
-					if(!validInt)
-						System.out.println("Nedarīga vērtība!");
-				}while(!validInt);
-				
-			}
-		}
+		int maxNameLength = Method_class.nameEntry(sklSk, sklName);
 		
-		//Izrēķina rezultātus
-		for(int i=0;i<sklSk;i++)
-		{
-			double sum=0;
-			for(int j=0; j<pdSk;j++)
-				sum += (double)pdRez[i][j]*((double)pdWg[j]/100);
-			finalRez[i] = sum;
-		}
+		Method_class.resEntry(pdSk, sklSk, sklName, pdWg, pdRez);
 		
-		//Gala rezultātu tabulas izvade
-		for(int i=0;i<maxNameLength+1;i++)
-			System.out.print(" ");
-		for(int i=0;i<pdSk;i++)
-			System.out.printf("%-17s", "Tests "+(i+1)+". ("+pdWg[i]+"%) ");
+		Method_class.calcFin(sklSk, pdSk, pdWg, finalRez, pdRez);
 		
-		System.out.println("Gala rez.");
-		for(int i=0;i<sklSk;i++)
-		{
-			System.out.print(sklName[i]+": ");
-			for(int j=0;j<maxNameLength-sklName[i].length();j++)
-				System.out.print(" ");
-			for(int j=0;j<pdSk;j++)
-				System.out.printf("%-17s", pdRez[i][j]);
-			System.out.println(df.format(finalRez[i]));
-		}
-		
+		Method_class.rezOut(maxNameLength, sklSk, pdSk, pdWg, finalRez, pdRez, sklName, df);
 		
 		sc.close();
 	}
--- src/studentuSvarigasAtzimes/Method_class.java
+++ src/studentuSvarigasAtzimes/Method_class.java
@@ -1,5 +1,6 @@
 package studentuSvarigasAtzimes;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Method_class {
@@ -124,6 +125,85 @@ public class Method_class {
 					}while(!validInt);
 				}
 		}
+		
+		//Skolēnu vārdu ievade ar blank validāciju
+		public static int nameEntry(int sklSk, String[] sklName) {
+			int maxNameLength =0;
+			sc.nextLine();
+			boolean validString;
+			for(int j=0;j<sklSk;j++) {
+				do {
+					System.out.println("Ievadiet "+(j+1)+". skolēna vārdu");
+					String tempInput = sc.nextLine();	
+					
+					validString = !tempInput.isBlank();
+					if(!validString)
+						System.out.println("Nedarīga vērtība!");
+					else {
+						sklName[j] = tempInput; 
+						maxNameLength = Math.max(maxNameLength, tempInput.length());
+					}
+				}while(!validString);
+			}
+			return maxNameLength;
+		}
+				
+		public static void resEntry(int pdSk, int sklSk, String[] sklName, int[] pdWg, int[][] pdRez) {
+			//Rezultātu ievade
+			for(int i=0;i<sklSk;i++)
+			{
+				for(int j=0;j<pdSk;j++)
+				{
+					System.out.println("Ievadi skolēna: "+sklName[i]+" atzīmi par "+(j+1)+". testu:\n"
+							+ "(svars "+pdWg[j]+")");
+					
+					pdRez[i][j] = 0;
+					do {
+						String tempInput = sc.next();
+						
+						if(isInteger(tempInput, 10))
+							pdRez[i][j] = Integer.parseInt(tempInput);
+						
+						validInt = (pdRez[i][j] < 1 || pdRez[i][j] > 10) ? false : true;
+							
+						if(!validInt)
+							System.out.println("Nedarīga vērtība!");
+					}while(!validInt);
+					
+				}
+			}
+		}
+		
+		public static void calcFin(int sklSk, int pdSk, int[] pdWg, double[] finalRez, int[][] pdRez) {
+			//Izrēķina rezultātus
+			for(int i=0;i<sklSk;i++)
+			{
+				double sum=0;
+				for(int j=0; j<pdSk;j++)
+					sum += (double)pdRez[i][j]*((double)pdWg[j]/100);
+				finalRez[i] = sum;
+			}
+		}
+		
+		public static void rezOut(int maxNameLength, int sklSk, int pdSk, int[] pdWg, double[] finalRez, int[][] pdRez, String[] sklName, DecimalFormat df) {
+			//Gala rezultātu tabulas izvade
+			for(int i=0;i<maxNameLength+1;i++)
+				System.out.print(" ");
+			for(int i=0;i<pdSk;i++)
+				System.out.printf("%-17s", "Tests "+(i+1)+". ("+pdWg[i]+"%) ");
+			
+			System.out.println("Gala rez.");
+			for(int i=0;i<sklSk;i++)
+			{
+				System.out.print(sklName[i]+": ");
+				for(int j=0;j<maxNameLength-sklName[i].length();j++)
+					System.out.print(" ");
+				for(int j=0;j<pdSk;j++)
+					System.out.printf("%-17s", pdRez[i][j]);
+				System.out.println(df.format(finalRez[i]));
+			}
+		}
+		
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
